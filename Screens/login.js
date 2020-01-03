45ad1b28293dc0bd4e4ba522b146902cd20c8caf
Umair Mustafa <umairhassaaan@gmail.com>
2020-01-03 12:16:21 +0500

    add login UI

--- Screens/login.js
+++ Screens/login.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Button } from 'native-base';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions, KeyboardAvoidingView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Intro(props) {
   console.disableYellowBox = true;
   return (
+    <KeyboardAvoidingView style = {{ flex: 1 }} behavior="padding">
     <View style={styles.container}>
-      <Text style={styles.containerText}>Login</Text>
-      <Button  style={{ backgroundColor: '#1A535C', padding:50 }} 
+      <Ionicons name="ios-chatboxes" size={100} color="black" />
+      <Text style = { styles.text }>Enter Email</Text>
+      <TextInput style = {styles.input}
+        underlineColorAndroid = "transparent"
+        placeholder = "Email"
+        placeholderTextColor = "grey"/>
+      <Text style = { styles.text }>Enter Password</Text>
+      <TextInput style = {styles.input}
+        underlineColorAndroid = "transparent"
+        placeholder = "Password"
+        placeholderTextColor = "grey"
+        secureTextEntry = 'true'/>
+      <Button  style={ styles.submitButton } 
         onPress={() => { props.navigation.navigate('Home') }}>
-        <Text style = {styles.getStartedButtonText}>Login!</Text>
+        <Text style = {styles.submitButtonText}>Submit</Text>
       </Button>
     </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -22,13 +36,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#FCFCFC',
   },
-  containerText: {
-    
-    color: '#1A535C',
-    fontSize: 50,
-    margin: 20,
-  },
-  getStartedButtonText: {
+  text: {
+    textAlign: 'left',
+    color: 'black',
+    padding: 10,
+ },
+ input: {
+    marginBottom: 25,
+    marginLeft: 5,
+    height: 40,
+    width: Dimensions.get('window').width / 1.1,
+    borderColor: 'white',
+    borderBottomColor: 'black',
+    borderWidth: 1,
+ },
+ submitButton: {
+    backgroundColor: '#1A535C',
+    padding: 20,
+    height: 40,
+    marginLeft:  Dimensions.get('window').width / 1.5,
+ },
+ submitButtonText:{
     color: 'white',
-  }
+    textAlign: 'center',
+ }
 });
